ebf45b94a237d203689798ab578a81d5599089b3
Hendriela <hendrik-heiser@gmx.de>
2021-10-13 15:57:29 +0200

    Send strings to serial port (connected Arduino).

---
+++ serial_write.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on 13/10/2021 15:56
+@author: hheise
+
+"""
+
+import serial
+import time
+
+# Configure serial port
+ser = serial.Serial()
+ser.baudrate = 19200
+ser.port = '/dev/ttyUSB0'
+
+# Open serial port
+ser.open()
+time.sleep(2.00) # Wait for connection before sending any data
+
+# Send a character
+ser.write(b'x')
+time.sleep(0.05)
+
+# Send a character
+ser.write(b'o')
+time.sleep(1.00)
+
+# Send a character
+ser.write(b'x')
+time.sleep(0.05)
+
+# Close serial port
+ser.close()
